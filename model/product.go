8823cc12773151ad789660cc2294bada516bb42d
bambam <abhishek.singh6646@gmail.com>
2021-11-29 11:34:06 +0530

    updates

--- model/product.go
+++ model/product.go
@@ -9,3 +9,11 @@ type Product struct {
 	Description string `gorm:"not null" json:"description"`
 	Amount      int    `gorm:"not null" json:"amount"`
 }
+
+// User struct
+type User struct {
+	gorm.Model
+	Name  string `gorm:"not null" json:"name"`
+	Email string `gorm:"not null" json:"email"`
+	//	Amount      int    `gorm:"not null" json:"amount"`
+}
